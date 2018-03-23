d64271c3c043ba708eb8439142d9c4bf27c076ac
Jordan Denison <jordan@denisonweb.com>
2018-03-23 13:33:52 -0500

    fix initial auth

--- src/lib/initialAuth.js
+++ src/lib/initialAuth.js
@@ -1,22 +1,6 @@
 import feathersCloudAuthentication from 'lib/feathers/cloud/feathersAuthentication'
 import feathersLocalAuthentication from 'lib/feathers/local/feathersAuthentication'
 
-const getParameterByName = (name, url) => {
-  if (!url) url = window.location.href
-  name = name.replace(/[[\]]/g, '\\$&')
-  const regex = new RegExp('[?&]' + name + '(=([^&#]*)|&|#|$)')
-  const results = regex.exec(url)
-  if (!results) return null
-  if (!results[2]) return ''
-  return decodeURIComponent(results[2].replace(/\+/g, ' '))
-}
-
-const queryStringAccessToken = getParameterByName('accessToken')
-
-const accessToken = queryStringAccessToken || window.localStorage && // eslint-disable-line
-                      window.localStorage.getItem && // eslint-disable-line
-                      window.localStorage.getItem('feathers-jwt') // eslint-disable-line
-
 const publicAuthData = {
   strategy: 'local',
   email: 'public@aboveboard.com',
@@ -29,34 +13,16 @@ const localAuthData = {
   password: 'local'
 }
 
-const init = store => {
-  if (accessToken) {
-    const authenticationOptions = {
-      strategy: 'jwt',
-      accessToken
-    }
-
-    return store.dispatch(feathersCloudAuthentication.authenticate(publicAuthData))
-      .then(() => store.dispatch(feathersLocalAuthentication.authenticate(authenticationOptions)))
-      .then(results => {
-        store.dispatch({
-          type: 'LOGIN_SUCCESS',
-          user: results.value.user,
-          accessToken: results.value.accessToken
-        })
+const init = store =>
+  store.dispatch(feathersCloudAuthentication.authenticate(publicAuthData))
+    .then(() => store.dispatch(feathersLocalAuthentication.authenticate(localAuthData)))
+    .then(results => {
+      store.dispatch({
+        type: 'LOGIN_SUCCESS',
+        user: results.value.user,
+        accessToken: results.value.accessToken
       })
-      .catch(() =>
-        store.dispatch(feathersLocalAuthentication.authenticate(localAuthData))
-          .then(results => {
-            store.dispatch({
-              type: 'LOGIN_SUCCESS',
-              user: results.value.user,
-              accessToken: results.value.accessToken
-            })
-          }))
-  } else {
-    return Promise.resolve()
-  }
-}
+    })
+    .catch(e => console.error(`Login error: ${e}`))
 
 export default init
