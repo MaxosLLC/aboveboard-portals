8807631a9fb334b587e2a10b209a97edebfc9af9
deni1688 <denisj1688@gmail.com>
2018-04-13 17:13:12 +0200

    bug fixes and additions to settings

--- src/components/header/HeaderView.js
+++ src/components/header/HeaderView.js
@@ -4,39 +4,16 @@ import Breadcrumbs from '../breadcrumbs/Breadcrumbs'
 import './Header.css'
 
 const profileSrc = './images/icons/defaultProfile.svg'
-const bellSrc = './images/icons/bell.svg'
-const searchSrc = './images/icons/search.svg'
 
 class HeaderView extends Component {
   render () {
     const {currentUser, routeTo, logout} = this.props
     const menuTrigger = <Image src={profileSrc} className='profileImage' />
-    const msgTrigger = <Image src={bellSrc} className='headerIcon' />
-    // mock messages data
-    let messages = [
-      {sender: 'Broker ABC', message: 'You have some new transactions'},
-      {sender: 'Broker ABC', message: 'You have some new transactions'}
-    ]
-    let messagesSeen = false
-    const messagesList = messages.map((msg, index) => (
-      <Dropdown.Item key={`message-${index}`}>
-        <p>{msg.message}</p>
-        <small className='sender'>From {msg.sender}</small>
-      </Dropdown.Item>
-      )
-    )
-
     return currentUser.id || currentUser._id
       ? (
         <Menu className='headerComponent'>
           <Breadcrumbs />
           <Menu.Menu position='right'>
-            <Menu.Item><Image src={searchSrc} className='headerIcon' /></Menu.Item>
-            <Dropdown pointing trigger={msgTrigger} icon={null} className={!messagesSeen ? 'msgActive' : ''}>
-              <Dropdown.Menu className='msgDropdownMenu'>
-                {messagesList}
-              </Dropdown.Menu>
-            </Dropdown>
             <Menu.Item>{currentUser.walletAccountName}</Menu.Item>
             <Dropdown pointing trigger={menuTrigger} icon={null}>
               <Dropdown.Menu className='headerDropdownMenu'>
