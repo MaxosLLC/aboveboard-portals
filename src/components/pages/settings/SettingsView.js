515f05adceb79e75fb5c760208a7cf5d6cfbb870
deni1688 <denisj1688@gmail.com>
2018-04-13 20:47:28 +0200

    rebased from dev x2

--- src/components/pages/settings/SettingsView.js
+++ src/components/pages/settings/SettingsView.js
@@ -166,7 +166,11 @@ class SettingsView extends Component {
               icon='search'
               className='settingInput'
               style={{
+<<<<<<< HEAD
                 width: '83%',
+=======
+                width: '84%',
+>>>>>>> rebased from dev x2
                 marginLeft: 0
               }} />
           </div>
