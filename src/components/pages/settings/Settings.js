d72a301afad62370f698e2d6b4411aa12c6fa889
Jordan Denison <jordan@denisonweb.com>
2018-04-19 14:05:07 -0500

    dispatch

--- src/components/pages/settings/Settings.js
+++ src/components/pages/settings/Settings.js
@@ -29,7 +29,7 @@ const mapDispatchToProps = (dispatch, ownProps) => {
         .then(() => dispatch(localServices.localToken.find()))
     },
     setMessagingAddress (messagingAddress, tokens) {
-      return localServices.user.patch(null, { messagingAddress }, { query: { email: 'local@local.com' } })
+      return dispatch(localServices.user.patch(null, { messagingAddress }, { query: { email: 'local@local.com' } }))
         .then(() => {
           if (appType === 'issuer') {
             return each(tokens, token => ethereum.setMessagingAddress(messagingAddress, token.address))
