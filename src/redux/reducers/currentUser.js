3b94325a3fbb29458f221c0ae167f902b0e3ecac
valeri <valeri.tsertsvadze.4@gmail.com>
2018-05-11 20:11:07 +0400

    update action in current user saga

--- src/redux/reducers/currentUser.js
+++ src/redux/reducers/currentUser.js
@@ -2,15 +2,23 @@ export default (state = {}, action) => {
   switch (action.type) {
     case 'LOGIN':
       return Object.assign({}, { isSaving: true })
+
     case 'LOGIN_SUCCESS':
     case 'SET_CURRENT_USER':
       return Object.assign({}, action.user)
+
     case 'SERVICES_USER_PATCH_FULFILLED':
       return Object.assign({}, state, action.payload)
+
     case 'LOGIN_ERROR':
       return { error: action.error.message }
+
     case 'LOGOUT':
       return {}
+
+    case 'UPDATE':
+      return {...state, updating: true}
+
     default:
       return state
   }
