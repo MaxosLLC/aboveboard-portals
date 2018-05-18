0f389eb2f543d83dacc0a870a2904a421f3eb7a8
Jordan Denison <jordan@denisonweb.com>
2018-05-18 10:42:38 -0500

    impl login with local API

--- src/redux/sagas/auth.js
+++ src/redux/sagas/auth.js
@@ -1,6 +1,6 @@
 import { all, put, takeLatest } from 'redux-saga/effects'
-import { push } from 'react-router-redux'
 import store from 'redux/store'
+import { push } from 'react-router-redux'
 import feathersLocalAuthentication from 'lib/feathers/local/feathersAuthentication'
 import cloudServices from 'lib/feathers/cloud/feathersServices'
 import localServices from 'lib/feathers/local/feathersServices'
@@ -13,9 +13,38 @@ const removeJwtFromLocalStorage = () => {
   }
 }
 
+function * login (params) {
+  const { email, password, rememberMe } = params.data
+
+  const data = {
+    strategy: 'local',
+    email,
+    password
+  }
+
+  try {
+    const results = yield store.dispatch(feathersLocalAuthentication.authenticate(data))
+
+    if (!rememberMe) {
+      removeJwtFromLocalStorage()
+    }
+
+    yield put({
+      type: 'LOGIN_SUCCESS',
+      user: results.value.user,
+      accessToken: results.value.accessToken
+    })
+  } catch (error) {
+    yield put({
+      type: 'LOGIN_ERROR',
+      error
+    })
+  }
+}
+
 function * loginSuccess ({ user, accessToken }) {
-  if (!user.messagingAddress && /(broker|issuer)/.test(appType)) {
-    yield store.dispatch(push('/settings'))
+  if (window.location.pathname === '/login') {
+    yield put(push('/'))
   }
 
   yield ethereum.init({
@@ -27,10 +56,10 @@ function * loginSuccess ({ user, accessToken }) {
 
   const [ethAddress] = yield ethereum.getAccounts()
 
-  yield store.dispatch(localServices.user.patch(null, { ethAddresses: [ { address: ethAddress } ] }, { query: { email: 'local@local.com' } }))
+  yield store.dispatch(localServices.user.patch(null, { ethAddresses: [ { address: ethAddress } ] }, { query: { email: user.email } }))
   yield store.dispatch(cloudServices.token.find())
   yield store.dispatch(localServices.localToken.find())
-  if (appType === 'broker' || appType === 'direct') {
+  if (appType === 'broker') {
     yield store.dispatch(cloudServices.whitelist.find())
   }
 }
@@ -44,6 +73,7 @@ function logout () {
 
 export default function * watchAuth () {
   yield all([
+    takeLatest('LOGIN', login),
     takeLatest('LOGIN_SUCCESS', loginSuccess),
     takeLatest('LOGOUT', logout)
   ])
--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import store from 'redux/store'
 import { Route, Switch } from 'react-router-dom'
 
+import Login from 'components/pages/login/Login'
 import Home from 'components/pages/home/Home'
 import Tokens from 'components/pages/tokens/Tokens'
 import TokenDetail from 'components/pages/tokens/detail/TokenDetail'
@@ -11,6 +12,7 @@ import ShareholderDetail from 'components/pages/tokens/detail/shareholders/detai
 import AddInvestor from 'components/pages/investors/add/AddInvestor'
 import EditInvestor from 'components/pages/investors/edit/EditInvestor'
 import Settings from 'components/pages/settings/Settings'
+import EnsureLoggedIn from 'components/auth/EnsureLoggedIn'
 
 class Routes extends Component {
   render () {
@@ -18,15 +20,18 @@ class Routes extends Component {
 
     return (
       <Switch>
-        <Route exact path='/' component={Home} />
-        <Route exact path='/settings' component={Settings} />
-        { appType === 'broker' || appType === 'direct' ? <Route exact path='/buyers' component={Investors} /> : '' }
-        { appType === 'broker' || appType === 'direct' ? <Route exact path='/buyers/add' component={AddInvestor} /> : '' }
-        { appType === 'broker' || appType === 'direct' ? <Route exact path='/buyers/:id/detail' component={InvestorDetail} /> : '' }
-        { appType === 'broker' || appType === 'direct' ? <Route exact path='/buyers/:id/edit' component={EditInvestor} /> : '' }
-        { appType === 'issuer' || appType === 'direct' ? <Route exact path='/tokens' component={Tokens} /> : '' }
-        { appType === 'issuer' || appType === 'direct' ? <Route exact path='/tokens/:address/detail' component={TokenDetail} /> : '' }
-        { appType === 'issuer' || appType === 'direct' ? <Route exact path='/tokens/:address/shareholders/:id/detail' component={ShareholderDetail} /> : '' }
+        <Route exact path='/login' component={Login} />
+        <EnsureLoggedIn>
+          <Route exact path='/' component={Home} />
+          <Route exact path='/settings' component={Settings} />
+          { appType === 'broker' || appType === 'direct' ? <Route exact path='/buyers' component={Investors} /> : '' }
+          { appType === 'broker' || appType === 'direct' ? <Route exact path='/buyers/add' component={AddInvestor} /> : '' }
+          { appType === 'broker' || appType === 'direct' ? <Route exact path='/buyers/:id/detail' component={InvestorDetail} /> : '' }
+          { appType === 'broker' || appType === 'direct' ? <Route exact path='/buyers/:id/edit' component={EditInvestor} /> : '' }
+          { appType === 'issuer' || appType === 'direct' ? <Route exact path='/tokens' component={Tokens} /> : '' }
+          { appType === 'issuer' || appType === 'direct' ? <Route exact path='/tokens/:address/detail' component={TokenDetail} /> : '' }
+          { appType === 'issuer' || appType === 'direct' ? <Route exact path='/tokens/:address/shareholders/:id/detail' component={ShareholderDetail} /> : '' }
+        </EnsureLoggedIn>
       </Switch>
     )
   }
