028c9b29148891900ed65cf70f4e56c3d491b93a
Jordan Denison <jordan@denisonweb.com>
2018-06-14 10:54:59 -0500

    add multisig to sidebar

--- src/components/sidebar/SidebarView.js
+++ src/components/sidebar/SidebarView.js
@@ -19,33 +19,35 @@ class SidebarView extends Component {
   render () {
     const { connected, currentUser, currentToken, routeTo, router } = this.props
 
+    const MenuItem = Menu.Item
+
     return currentUser.id || currentUser._id ? (
       <Menu inverted vertical className='sidebarComponent'>
         <MenuItem onClick={() => routeTo('/')} className='logoContainer'>
           <Image src={logoSrc} className='siteLogo' />
           { /* <p className='version'>{ REACT_APP_VERSION || '1.0.0' }</p> */ }
         </MenuItem>
-        { currentUser.role === 'buyer' &&
-          <MenuItem name='buyers' onClick={() => routeTo(`/buyers/your-info`)} active={buyersRegexp.test(router.location.pathname)} className='sidebarMenuItem'>
-            <span><Image src={dollarSignSrc} className='menuIcon' />Your Info</span><Image src={sortArrowsSrc} className='menuIcon-sm' />
-          </MenuItem>
-        }
-        { /(broker|direct)/.test(currentUser.role) &&
-          <MenuItem name='buyers' onClick={() => routeTo('/buyers')} active={buyersRegexp.test(router.location.pathname)} className='sidebarMenuItem'>
+        { currentUser.role === 'broker' || currentUser.role === 'direct'
+          ? <MenuItem name='buyers' onClick={() => routeTo('/buyers')} active={buyersRegexp.test(router.location.pathname)} className='sidebarMenuItem'>
             <span><Image src={dollarSignSrc} className='menuIcon' />Buyers</span><Image src={sortArrowsSrc} className='menuIcon-sm' />
           </MenuItem>
-        }
-        { /(issuer|direct)/.test(currentUser.role) &&
-          <MenuItem name='tokens' onClick={() => routeTo('/tokens')} active={tokensRegexp.test(router.location.pathname)} className='sidebarMenuItem'>
+        : null }
+        { currentUser.role === 'issuer' || currentUser.role === 'direct'
+          ? <MenuItem name='tokens' onClick={() => routeTo('/tokens')} active={tokensRegexp.test(router.location.pathname)} className='sidebarMenuItem'>
             <span><Image src={dollarSignSrc} className='menuIcon' />Securities</span><Image src={sortArrowsSrc} className='menuIcon-sm' />
           </MenuItem>
-        }
+        : null }
         <MenuItem
           active={tokenDetailRegexp.test(router.location.pathname)}
           className='sidebarMenuItem'
           onClick={currentToken ? () => routeTo(`/tokens/${currentToken}/detail`) : null}>
           <span><Image src={barsSrc} className='menuIcon' />Dashboard</span>
         </MenuItem>
+        { currentUser.role === 'issuer' || currentUser.role === 'direct' || currentUser.role === 'broker'
+          ? <MenuItem name='multisigwallets' onClick={() => routeTo('/multi-signature-wallets')} active={tokensRegexp.test(router.location.pathname)} className='sidebarMenuItem'>
+            <span><Image src={dollarSignSrc} className='menuIcon' />Multi Signature Wallets</span><Image src={sortArrowsSrc} className='menuIcon-sm' />
+          </MenuItem>
+        : null }
         <MenuItem className='sidebarMenuItem'>
           <span><Image src={walletSrc} className='menuIcon' />Wallet</span>
           <span className={connected ? 'connected' : 'disconnected'} />
