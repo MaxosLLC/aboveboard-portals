dd30ebfec81d4b0e269f61ea900355f8f3c19512
Jordan Denison <jordan@denisonweb.com>
2018-07-11 13:53:28 -0500

    lint and remove log

--- src/components/update/index.js
+++ src/components/update/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { Message } from 'semantic-ui-react'
+// import { Message } from 'semantic-ui-react'
 import { connect } from 'react-redux'
 
 import './update.css'
