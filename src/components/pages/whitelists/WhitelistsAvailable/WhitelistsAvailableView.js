36b4ad640415046707630c302a7b4e68cc9c27a6
Jordan Denison <jordan@denisonweb.com>
2018-08-17 14:51:44 -0500

    change header

--- src/components/pages/whitelists/WhitelistsAvailable/WhitelistsAvailableView.js
+++ src/components/pages/whitelists/WhitelistsAvailable/WhitelistsAvailableView.js
@@ -49,7 +49,7 @@ class WhitelistsAvailableView extends Component {
     return (
       <div className='whitelistsComponent'>
         <Header as='h2'>Token: Aboveboard Demo Token</Header>
-        <Header as='h3'>More information about this product {'<'}Update Link{'>'}</Header>
+        <Header as='h3'>More information about this project <Button style={{ marginLeft: '10px' }}>Add A Link</Button></Header>
         <Grid style={{ marginTop: '10px' }}>
           <Grid.Column floated='left' width={5}>
             <Header as='h2'>Available Whitelists</Header>
