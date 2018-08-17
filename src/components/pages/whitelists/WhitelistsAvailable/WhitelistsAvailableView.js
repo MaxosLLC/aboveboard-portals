b569d4a727069a8f94a2671f67ea7e095589e285
Jordan Denison <jordan@denisonweb.com>
2018-08-17 14:41:19 -0500

    fix table style

--- src/components/pages/whitelists/WhitelistsAvailable/WhitelistsAvailableView.js
+++ src/components/pages/whitelists/WhitelistsAvailable/WhitelistsAvailableView.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import { each } from 'bluebird'
-import { Button, Header, Icon, Input, Pagination, Segment, Table } from 'semantic-ui-react'
+import { Button, Grid, Header, Icon, Image, Input, Segment, Table } from 'semantic-ui-react'
 
 import ethereum from 'lib/ethereum'
 import './WhitelistsAvailable.css'
 
-class PendingTransactionsView extends Component {
+const iconsPath = '/images/icons'
+const sortUpSrc = `${iconsPath}/up.svg`
+const sortDownSrc = `${iconsPath}/down.svg`
+
+class WhitelistsAvailableView extends Component {
   constructor (props) {
     super(props)
     this.state = {
@@ -40,106 +44,95 @@ class PendingTransactionsView extends Component {
   }
 
   render () {
-    const { loaded, whitelists, setPage, setSearch, page, search, queryResult } = this.props
-
-    const whitelistHeaders = [
-      { name: 'Name', sortOption: 'name' },
-      { name: 'Qualifier', sortOption: 'qualifier' },
-      { name: 'Type', sortOption: 'type' },
-      { name: 'Address', sortOption: 'address' },
-      { name: 'Actions' }
-    ]
+    const { loaded, whitelists, setSearch, search } = this.props
 
     return (
       <div className='whitelistsComponent'>
         <Header as='h2'>Token: Aboveboard Demo Token</Header>
-        <br />
-        <Header as='h2'>Available Whitelists</Header>
-        <div>
-          <Input loading={!loaded} icon='dollar' placeholder='Search...' onChange={(e, { value }) => { setSearch(value) }} value={search} />
-        </div>
-        <br />
+        <Header as='h3'>More information about this product {'<'}Update Link{'>'}</Header>
+        <Grid style={{ marginTop: '10px' }}>
+          <Grid.Column floated='left' width={5}>
+            <Header as='h2'>Available Whitelists</Header>
+          </Grid.Column>
+          <Grid.Column floated='right' width={5}>
+            <Input style={{ position: 'relative', top: '-10px', color: 'black' }} loading={!loaded} icon='dollar' placeholder='Search...' onChange={(e, { value }) => { setSearch(value) }} value={search} />
+          </Grid.Column>
+        </Grid>
         {!loaded
           ? <span>Loading whitelists...<Icon name='spinner' loading /></span>
           : whitelists.length
           ? <div className='tableContainer'>
-            <div className='tableScrollContainer'>
-              <Table className='abTable' unstackable>
-                <Table.Header className='tableHeader'>
-                  <Table.Row>
-                    <Table.HeaderCell>Name</Table.HeaderCell>
-                    <Table.HeaderCell>Qualifier</Table.HeaderCell>
-                    <Table.HeaderCell>Type</Table.HeaderCell>
-                    <Table.HeaderCell>Actions</Table.HeaderCell>
-                  </Table.Row>
-                </Table.Header>
+            <Table className='abTable' unstackable compact>
+              <Table.Header className='tableHeader'>
+                <Table.Row>
+                  <Table.HeaderCell>Name <span className='sortButtons'>
+                    <Image
+                      src={sortUpSrc} />
+                    <Image
+                      src={sortDownSrc} />
+                  </span>
+                  </Table.HeaderCell>
+                  <Table.HeaderCell>Qualifier <span className='sortButtons'>
+                    <Image
+                      src={sortUpSrc} />
+                    <Image
+                      src={sortDownSrc} />
+                  </span></Table.HeaderCell>
+                  <Table.HeaderCell>Type <span className='sortButtons'>
+                    <Image
+                      src={sortUpSrc} />
+                    <Image
+                      src={sortDownSrc} />
+                  </span></Table.HeaderCell>
+                  <Table.HeaderCell>Actions</Table.HeaderCell>
+                </Table.Row>
+              </Table.Header>
 
-                <Table.Body>
-                  <Table.Row>
-                    <Table.Cell>Company Affiliates</Table.Cell>
-                    <Table.Cell>Company</Table.Cell>
-                    <Table.Cell>Affiliate</Table.Cell>
-                    <Table.Cell />
-                  </Table.Row>
-                  <Table.Row>
-                    <Table.Cell>Company Direct Sales Portal</Table.Cell>
-                    <Table.Cell>Company</Table.Cell>
-                    <Table.Cell>Accredited</Table.Cell>
-                    <Table.Cell><Button>Request Your Portal</Button></Table.Cell>
-                  </Table.Row>
-                  <Table.Row>
-                    <Table.Cell>Aboveboard US QIB</Table.Cell>
-                    <Table.Cell>Aboveboard</Table.Cell>
-                    <Table.Cell>QIB</Table.Cell>
-                    <Table.Cell><Button>Request Distribution</Button></Table.Cell>
-                  </Table.Row>
-                  <Table.Row>
-                    <Table.Cell>US Broker-Dealer</Table.Cell>
-                    <Table.Cell>A Broker</Table.Cell>
-                    <Table.Cell>US Accredited</Table.Cell>
-                    <Table.Cell><Button>Request Distribution</Button></Table.Cell>
-                  </Table.Row>
-                  <Table.Row>
-                    <Table.Cell>Japan Broker</Table.Cell>
-                    <Table.Cell>Japan Broker</Table.Cell>
-                    <Table.Cell>Sophisticated</Table.Cell>
-                    <Table.Cell><Button>Request Distribution</Button></Table.Cell>
-                  </Table.Row>
-                  <Table.Row>
-                    <Table.Cell>Exchange 1</Table.Cell>
-                    <Table.Cell>Exchange 1</Table.Cell>
-                    <Table.Cell>Reg S</Table.Cell>
-                    <Table.Cell><Button>Request Distribution</Button></Table.Cell>
-                  </Table.Row>
-                  <Table.Row>
-                    <Table.Cell>Exchange 2</Table.Cell>
-                    <Table.Cell>Exchange 2</Table.Cell>
-                    <Table.Cell>Sophisticated</Table.Cell>
-                    <Table.Cell><Button>Request Distribution</Button></Table.Cell>
-                  </Table.Row>
-                </Table.Body>
-
-                <Table.Footer>
-                  <Table.Row>
-                    <Table.HeaderCell floated='right' colSpan={whitelistHeaders.length}>
-                      <Pagination
-                        floated='right'
-                        activePage={page + 1}
-                        totalPages={
-                          queryResult.whitelists
-                            ? Math.floor(
-                                queryResult.whitelists.total /
-                                  queryResult.whitelists.limit
-                              ) + 1
-                            : 1
-                        }
-                        onPageChange={(e, { activePage }) => setPage(activePage - 1)}
-                      />
-                    </Table.HeaderCell>
-                  </Table.Row>
-                </Table.Footer>
-              </Table>
-            </div>
+              <Table.Body>
+                <Table.Row>
+                  <Table.Cell>Company Affiliates</Table.Cell>
+                  <Table.Cell>Company</Table.Cell>
+                  <Table.Cell>Affiliate</Table.Cell>
+                  <Table.Cell />
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell>Company Direct Sales Portal</Table.Cell>
+                  <Table.Cell>Company</Table.Cell>
+                  <Table.Cell>Accredited</Table.Cell>
+                  <Table.Cell><Button>Request Your Portal</Button></Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell>Aboveboard US QIB</Table.Cell>
+                  <Table.Cell>Aboveboard</Table.Cell>
+                  <Table.Cell>QIB</Table.Cell>
+                  <Table.Cell><Button>Request Distribution</Button></Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell>US Broker-Dealer</Table.Cell>
+                  <Table.Cell>A Broker</Table.Cell>
+                  <Table.Cell>US Accredited</Table.Cell>
+                  <Table.Cell><Button>Request Distribution</Button></Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell>Japan Broker</Table.Cell>
+                  <Table.Cell>Japan Broker</Table.Cell>
+                  <Table.Cell>Sophisticated</Table.Cell>
+                  <Table.Cell><Button>Request Distribution</Button></Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell>Exchange 1</Table.Cell>
+                  <Table.Cell>Exchange 1</Table.Cell>
+                  <Table.Cell>Unregulated Only</Table.Cell>
+                  <Table.Cell><Button>Request Distribution</Button></Table.Cell>
+                </Table.Row>
+                <Table.Row>
+                  <Table.Cell>Exchange 2</Table.Cell>
+                  <Table.Cell>Exchange 2</Table.Cell>
+                  <Table.Cell>Sophisticated</Table.Cell>
+                  <Table.Cell><Button>Request Distribution</Button></Table.Cell>
+                </Table.Row>
+              </Table.Body>
+            </Table>
           </div>
           : <Segment>{ search ? 'No whitelists match your search criteria' : 'No whitelists have been made yet' }</Segment>
         }
@@ -148,4 +141,4 @@ class PendingTransactionsView extends Component {
   }
 }
 
-export default PendingTransactionsView
+export default WhitelistsAvailableView
