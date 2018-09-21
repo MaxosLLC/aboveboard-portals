b1a64b864e455d3e670c111cbca446fd20419329
Jordan Denison <jordan@denisonweb.com>
2018-09-21 14:00:09 -0500

    fix token

--- src/lib/initialAuth.js
+++ src/lib/initialAuth.js
@@ -13,7 +13,7 @@ const getParameterByName = (name, url) => {
 
 const queryStringAccessToken = getParameterByName('accessToken')
 
-const letmeinAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1YmE1Mzk5YjVlZTU2MDAwNjZlZTc1MjEiLCJpYXQiOjE1Mzc1NTYyMTgsImV4cCI6MTU2OTA5MjIxOCwiYXVkIjoiaHR0cHM6Ly9pbnZlc3QuYWJvdmVib2FyZC5haSIsInN1YiI6Imluc2VjdXJlLXRva2VuIiwianRpIjoiNzlkYTJjM2YtY2E2YS00ODdhLWE3N2QtMTI4NTk4MzcwNjQ3In0.zUNXP8ry7sI-6HfrfiiAWAlEDrGwGnnoUf-MBPA5a64'
+const letmeinAccessToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiI1YmE1Mzk5YjVlZTU2MDAwNjZlZTc1MjEiLCJpYXQiOjE1Mzc1NTYzOTEsImV4cCI6MTU2OTA5MjM5MSwiYXVkIjoiaHR0cHM6Ly9pbnZlc3QuYWJvdmVib2FyZC5haSIsImlzcyI6ImZlYXRoZXJzIiwic3ViIjoiQWJvdmVib2FyZCIsImp0aSI6ImQ4YTE3M2VjLWFmMWMtNDJkZS1hMjY2LTY3NzRkN2ZkOWFiMCJ9.URO7Ej2oqwp-q4qRlLoFh_d7-vltHwTzJPymJ7qHYh4'
 
 const accessToken = window.location.pathname === '/letmein' ? letmeinAccessToken : (queryStringAccessToken || (window.localStorage && window.localStorage.getItem && window.localStorage.getItem('local-feathers-jwt')))
 
