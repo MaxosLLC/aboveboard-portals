6b0fbfdcd4794db16c42013a20780ba822128cd0
Jordan Denison <jordan@denisonweb.com>
2018-10-01 10:52:53 -0500

    point to issuer api for direct for local

--- src/lib/feathers/local/feathersClient.js
+++ src/lib/feathers/local/feathersClient.js
@@ -8,7 +8,7 @@ const appType = /issuer/.test(window.location.hostname) ? 'issuer' : /direct/.te
 export const url = window.REACT_APP_APP_TYPE ? `${window.location.protocol}//${window.location.host}/local-api/`
   : process.env.REACT_APP_NODE_ENV === 'test' ? process.env[`REACT_APP_${appType.toUpperCase()}_LOCAL_API_TEST_URL`]
   : appType === 'broker' ? process.env.REACT_APP_BROKER_LOCAL_API_URL || 'https://aboveboard-broker-api.herokuapp.com/'
-  : appType === 'direct' ? process.env.REACT_APP_DIRECT_LOCAL_API_URL || 'https://aboveboard-direct-api.herokuapp.com/'
+  : appType === 'direct' || appType === 'issuer' ? process.env.REACT_APP_DIRECT_LOCAL_API_URL || 'https://aboveboard-issuer-api.herokuapp.com/'
   : process.env.REACT_APP_ISSUER_LOCAL_API_URL || 'https://aboveboard-issuer-api.herokuapp.com/'
 
 const socket = io(url.replace(/local-api\//, ''), {
