695f6f2349aafe1ac7c59c814cbbe7e32fd9fec4
Jordan Denison <jordan@denisonweb.com>
2018-11-04 16:34:07 -0600

    disable ethereum network checker for now

--- src/redux/sagas/ethereumNetworkChecker.js
+++ src/redux/sagas/ethereumNetworkChecker.js
@@ -1,14 +1,14 @@
 import { all, takeLatest } from 'redux-saga/effects'
 
 import ethereum from 'lib/ethereum'
-import store from 'redux/store'
+// import store from 'redux/store'
 
 function * checkCurrentNetwork () {
   const currentNetwork = yield ethereum.getCurrentNetwork()
-console.log('cn ', currentNetwork)
-  if (/kovan/.test(window.location.hostname) && +currentNetwork !== 42) {
-    store.dispatch({ type: 'WALLET_TRANSACTION_START', method: 'changeToKovanNetwork' })
-  }
+  console.log('current wallet ethereum network ', currentNetwork)
+  // if (/kovan/.test(window.location.hostname) && +currentNetwork !== 42) {
+  //   store.dispatch({ type: 'WALLET_TRANSACTION_START', method: 'changeToKovanNetwork' })
+  // }
 }
 
 export default function * watchAuth () {
